b3ecafa673d2fa788ca07d38ca3cfdaf311b6d77
alejandrogt-gg <46728799+alejandrogt-gg@users.noreply.github.com>
2019-11-27 15:35:59 -0500

    adding a static constructor to set the random seed

--- ComWriter/RandomUtility.h
+++ ComWriter/RandomUtility.h
@@ -4,6 +4,13 @@
 
 class RandomUtility
 {
+private:
+	friend class StaticConstructor;
+	struct StaticConstructor {
+		StaticConstructor();
+	};
+
+	static StaticConstructor _StaticConstructor;
 public:
 	static int Range(int low, int high);
 	static char Character();
